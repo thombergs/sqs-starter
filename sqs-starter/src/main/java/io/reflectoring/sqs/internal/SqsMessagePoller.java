df7fd04eb7e246c2aba4cba9f521cbf54603c48c
Tom Hombergs <tom.hombergs@gmail.com>
2020-04-29 20:26:16 +1000

    Add error log when poller cannot fetch messages

--- sqs-starter/src/main/java/io/reflectoring/sqs/internal/SqsMessagePoller.java
+++ sqs-starter/src/main/java/io/reflectoring/sqs/internal/SqsMessagePoller.java
@@ -23,60 +23,64 @@ import java.util.concurrent.TimeUnit;
 @RequiredArgsConstructor
 class SqsMessagePoller<T> {
 
-  private static final Logger logger = LoggerFactory.getLogger(SqsMessagePoller.class);
-  private final String name;
-  private final SqsMessageHandler<T> messageHandler;
-  private final SqsMessageFetcher messageFetcher;
-  private final SqsMessagePollerProperties pollingProperties;
-  private final AmazonSQS sqsClient;
-  private final ObjectMapper objectMapper;
-  private final ScheduledThreadPoolExecutor pollerThreadPool;
-  private final ThreadPoolExecutor handlerThreadPool;
+    private static final Logger logger = LoggerFactory.getLogger(SqsMessagePoller.class);
+    private final String name;
+    private final SqsMessageHandler<T> messageHandler;
+    private final SqsMessageFetcher messageFetcher;
+    private final SqsMessagePollerProperties pollingProperties;
+    private final AmazonSQS sqsClient;
+    private final ObjectMapper objectMapper;
+    private final ScheduledThreadPoolExecutor pollerThreadPool;
+    private final ThreadPoolExecutor handlerThreadPool;
 
-  void start() {
-    logger.info("starting SqsMessagePoller");
-    for (int i = 0; i < pollerThreadPool.getCorePoolSize(); i++) {
-      logger.info("starting SqsMessagePoller ({}) - thread {}", this.name, i);
-      pollerThreadPool.scheduleWithFixedDelay(
-          this::poll,
-          1,
-          pollingProperties.getPollDelay().toSeconds(),
-          TimeUnit.SECONDS);
+    void start() {
+        logger.info("starting SqsMessagePoller");
+        for (int i = 0; i < pollerThreadPool.getCorePoolSize(); i++) {
+            logger.info("starting SqsMessagePoller ({}) - thread {}", this.name, i);
+            pollerThreadPool.scheduleWithFixedDelay(
+                    this::pollMessages,
+                    1,
+                    pollingProperties.getPollDelay().toSeconds(),
+                    TimeUnit.SECONDS);
+        }
     }
-  }
 
-  void stop() {
-    logger.info("stopping SqsMessagePoller");
-    pollerThreadPool.shutdownNow();
-    handlerThreadPool.shutdownNow();
-  }
-
-  void poll() {
-
-    List<Message> messages = messageFetcher.fetchMessages();
-
-    for (Message sqsMessage : messages) {
-      try {
-        final T message = objectMapper.readValue(sqsMessage.getBody(), messageHandler.messageType());
-        handlerThreadPool.submit(() -> {
-          try{
-            messageHandler.handle(message);
-            acknowledgeMessage(sqsMessage);
-          } catch (Exception e) {
-            logger.warn("error while processing message {} - message will be retried according to SQS properties:", sqsMessage.getMessageId(), e);
-          }
-          logger.debug("message {} processed successfully - message has been deleted from SQS", sqsMessage.getMessageId());
-        });
-      } catch (JsonProcessingException e) {
-        logger.warn("error parsing message {} - deleting message from SQS because it's not recoverable: ", sqsMessage.getMessageId(), e);
-      }
-
-      // we let other exceptions bubble up to trigger a retry
+    void stop() {
+        logger.info("stopping SqsMessagePoller");
+        pollerThreadPool.shutdownNow();
+        handlerThreadPool.shutdownNow();
     }
-  }
 
-  private void acknowledgeMessage(Message message) {
-    sqsClient.deleteMessage(pollingProperties.getQueueUrl(), message.getReceiptHandle());
-  }
+    void pollMessages() {
+        try {
+            List<Message> messages = messageFetcher.fetchMessages();
+            for (Message sqsMessage : messages) {
+                handleMessage(sqsMessage);
+            }
+        } catch (Exception e) {
+            logger.error("error fetching messages from queue {}:", pollingProperties.getQueueUrl(), e);
+        }
+    }
+
+    private void handleMessage(Message sqsMessage) {
+        try {
+            final T message = objectMapper.readValue(sqsMessage.getBody(), messageHandler.messageType());
+            handlerThreadPool.submit(() -> {
+                try {
+                    messageHandler.handle(message);
+                    acknowledgeMessage(sqsMessage);
+                } catch (Exception e) {
+                    logger.warn("error while processing message {} - message will be retried according to SQS properties:", sqsMessage.getMessageId(), e);
+                }
+                logger.debug("message {} processed successfully - message has been deleted from SQS", sqsMessage.getMessageId());
+            });
+        } catch (JsonProcessingException e) {
+            logger.warn("error parsing message {} - deleting message from SQS because it's not recoverable: ", sqsMessage.getMessageId(), e);
+        }
+    }
+
+    private void acknowledgeMessage(Message message) {
+        sqsClient.deleteMessage(pollingProperties.getQueueUrl(), message.getReceiptHandle());
+    }
 
 }
